746f12d9836c7d5ab72a4fe1339e4c7004d28518
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-07-04 10:41:12 +0530

    Modules tut

---
+++ modules/hello.js
@@ -0,0 +1,6 @@
+// Dividing whole program into small-small module known as Modules.
+
+// import code using require build-in function.
+const { add, sub } = require("./math");
+
+console.log("Math value is", add(2, 5));
